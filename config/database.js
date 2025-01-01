ed89ee2a86f4757d88e918db874181dfc6024c94
Lamon <41296515+lamonst@users.noreply.github.com>
2025-01-01 09:54:36 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,8 @@
-// module.exports = {
-//   username: 'root',
-//   password: 'M@rl3y#1',
-//   database: 'products_db',
-//   host: '127.0.0.1',
-//   port: '3306',
-//   dialect: 'mysql',
-// }
-
-module.exports = {
-  username: 'wallet',
-  password: 'W@allet20',
-  database: 'wallet_dev',
-  host: '177.12.76.75',
-  port: '3306',
-  dialect: 'mysql'
-}
+ module.exports = {
+   username: 'root',
+   password: 'pwd',
+   database: 'products_db',
+   host: '127.0.0.1',
+   port: '3306',
+   dialect: 'mysql',
+ }
